a752a7814dc864f3247e9bddd244c3d831d2db86
Your Name <you@example.com>
2025-05-18 18:37:03 +0500

    next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['i.postimg.cc'], // Add the external domain for images
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
